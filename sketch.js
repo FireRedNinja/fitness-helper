8893c3bb51f1e482ae060d7a7e7261e996c8b6fb
Christopher Pauly <2196151P@student.gla.ac.uk>
2017-11-10 13:40:03 +0000

    Fixed the file. Added a test button that changes the background colour.

--- sketch.js
+++ sketch.js
@@ -1,9 +1,8 @@
-
-button = new button();
-
 function setup(){
-  button.onclick(changeBackground());
-
+  createCanvas(600,600);
+  background(51);
+  button = createButton("Test Button")
+  button.mousePressed(changeBackground);
 }
 function draw(){
 
@@ -11,5 +10,7 @@ function draw(){
 
 function changeBackground(){
   colour = random(0,255);
-  background(colour);
+  colour2 = random(0,255);
+  colour3 = random(0,255);
+  background(colour,colour2,colour3);
 }
