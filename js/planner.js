d01fb242338e8dfffe8fbabf6dac5eb4b4bc7807
Darya <2198278S@student.gla.ac.uk>
2017-11-17 20:31:04 +0000

    planner is now a dropdown menu

---
+++ js/planner.js
@@ -0,0 +1,6 @@
+$(document).ready = function () {
+    console.log("fuckity fuck");
+    $('.button-collapse').sideNav();
+    $('select').material_select();
+    $(".dropdown-button").dropdown();
+}
