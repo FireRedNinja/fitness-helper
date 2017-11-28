e25f55b20ad1d1d5e039decc1d62cc0f5b904ed5
Noel <leonr13@hotmail.co.uk>
2017-11-28 21:01:24 +0000

    google maps working

--- js/GPXViewer.js
+++ js/GPXViewer.js
@@ -1,44 +1,6 @@
-var map;
-
-function initMap() {
-    var uluru = {
-        lat: -25.363,
-        lng: 131.044
-    };
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
-        center: uluru
-    });
-    var marker = new google.maps.Marker({
-        position: uluru,
-        map: map
-    });
-}
-
-
 $(document).ready(function() {
 
-    $("#input").change(function(e) {
 
-        for (var i = 0; i < e.originalEvent.srcElement.files.length; i++) {
-
-            var file = e.originalEvent.srcElement.files[i];
-
-            var img = document.createElement("img");
-            var reader = new FileReader();
-            reader.onloadend = function() {
-                img.src = reader.result;
-            }
-            reader.readAsDataURL(file);
-            $("input").after(img);
-        }
-    });
-
-
-
-    $('#button').on('click', function() {
-        $('#file-input').trigger('click');
-    });
 
     $('#file-input').change(function() {
 
