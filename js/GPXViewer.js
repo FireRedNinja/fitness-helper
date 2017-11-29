4ecc8764e16756b5f9c9e4a4ae8b97ed02de2b97
Noel <leonr13@hotmail.co.uk>
2017-11-29 21:36:10 +0000

    made an attempt with the elevation chart

--- js/GPXViewer.js
+++ js/GPXViewer.js
@@ -18,6 +18,9 @@ $(document).ready(function() {
 
 
     $('#file-input').change(function() {
+        var elevation = [];
+        var heartrate = [];
+        var timeArray = [];
 
         var reader = new FileReader();
         reader.onload = function(e) {
@@ -34,6 +37,7 @@ $(document).ready(function() {
             console.log($name.text());
 
             $('#file-title').text($name.text());
+
 
 
             var totalTracks = 0;
@@ -65,6 +69,10 @@ $(document).ready(function() {
 
                 var elev = $(this).find('ele').text();
 
+                var gpxTime = $(this).find('time').text();
+
+                var time = new Date(gpxTime);
+
 
                 totalTracks += 1;
                 totalHR += parseInt(hr);
@@ -72,6 +80,10 @@ $(document).ready(function() {
                 totalLat += parseFloat(lat);
                 totalLon += parseFloat(lon);
                 totalElev += parseFloat(elev);
+
+                elevation.push(parseFloat(elev));
+                heartrate.push(parseFloat(hr));
+                timeArray.push(parseFloat(time.getTime())/1000/60);
 
                 //  Get the figures for the bounding box
                 if (maxLat == null || maxLon == null || minLat == null || minLon == null) {
@@ -127,6 +139,50 @@ $(document).ready(function() {
 
         };
 
+        console.log("time");
+        console.log(timeArray);
+        console.log("elevation");
+        console.log(elevation);
+
+        // Chartjs
+        var ctx = document.getElementById("myChart").getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: timeArray,
+                datasets: [{
+                    label: '# of Votes',
+                    data: elevation,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+        // end of chartjs
         reader.readAsText(this.files[0]);
         $('#file-preview').text(this.files[0].name);
 
