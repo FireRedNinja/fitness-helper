f6c5207b82736ab183d9361684517cebcc764613
Noel <leonr13@hotmail.co.uk>
2017-11-30 11:51:54 +0000

    Auto stash before merge of "master" and "origin/master"

--- js/GPXViewer.js
+++ js/GPXViewer.js
@@ -80,7 +80,7 @@ $(document).ready(function() {
                 totalLon += parseFloat(lon);
                 totalElev += parseFloat(elev);
 
-                elevation.push(parseFloat(elev));
+                elevation.push(Math.round(parseFloat(elev)));
                 heartrate.push(parseFloat(hr));
                 timeArray.push(time.getHours()+":"+time.getMinutes()+":"+time.getSeconds());
 
@@ -145,6 +145,7 @@ $(document).ready(function() {
         
 
         // Chartjs
+
         var ctx = document.getElementById("myChart").getContext('2d');
         var myChart = new Chart(ctx, {
             type: 'line',
@@ -159,7 +160,7 @@ $(document).ready(function() {
             },
             options: {
                 scales: {
-                    
+
                     yAxes: [{
                         ticks: {
                             beginAtZero: false
