37c34035c6465f63f833b0b58ee9b28c1a1d5d58
Noel <leonr13@hotmail.co.uk>
2017-11-30 18:59:59 +0000

    add unique ids to goals

--- tracy/js/Goals.js
+++ tracy/js/Goals.js
@@ -1,5 +1,10 @@
+$(document).ready = function () {
+   var uniqieid = 50;
+}
+
+
 $("#submitGoal").click( function() {
-    var goal = '<li class="collection-item avatar" id="goal1"><img src="assets/logo.png" alt="" class="circle"><h5>' + $('#workout-plan').val() +  '</h5><a href="#!" class="secondary-content"><i class="material-icons">delete</i></a></li>';
+    var goal = '<li class="collection-item avatar" id="' + uniqieid + '"><img src="assets/logo.png" alt="" class="circle"><h5>' + $('#workout-plan').val() +  '</h5><a id="' + uniqieid + '"href="#!" class="secondary-content"><i class="material-icons">delete</i></a></li>';
     $('#submitGoal').click(
         $(".collection").append(goal)
     );
